d0df3b8cb57c1d81a5e40b8a762a94fc7585a1a0
Zamaster <Batterybat@gmail.com>
2018-02-28 23:26:41 -0800

    Added the start of basic drawing classes

---
+++ dev/src/retro/fbgfx.h
@@ -0,0 +1,24 @@
+#ifndef RETRO_FBGFX_H_
+
+#include <memory>
+
+#include "glm/vec2.hpp"
+
+namespace retro {
+
+class FbGfx final {
+ public:
+  static std::unique_ptr<FbGfx> CreateScreen(glm::ivec2 dimensions);
+
+  // suite of draw functions, will also need some rudamentary SDL image wrapper.
+
+  void WaitForVSync() const;
+  
+  void Flip();
+ private:
+  FbGfx(glm::ivec2 dimensions);
+};
+
+}  // namespace retro
+
+#endif  // RETRO_FBGFX_H_
---
+++ dev/src/retro/fbimg.h
@@ -0,0 +1,14 @@
+#ifndef RETRO_FBIMG_H_
+
+
+#include "glm/vec2.hpp"
+
+namespace retro {
+
+class FbImg {
+
+};
+
+}  // namespace retro
+
+#endif  // RETRO_FBIMG_H_
